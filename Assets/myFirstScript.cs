69f2af38b3885298924c9d2d7749b7c0e11f0536
smallfri321 <63439547+smallfri321@users.noreply.github.com>
2020-09-07 17:39:41 -0500

    Hello world code
    first project

---
+++ Assets/myFirstScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class myFirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Debug.Log("Hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UnityEngine.Debug.Log("Hello world");
+    }
+
+    void Awake()
+    {
+        UnityEngine.Debug.Log("Hello World"); 
+    }
+}
